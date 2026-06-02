e64329ace24957086b9ce390a7fa39fca6f761fb
jiexun273 <lijiexun273@gmail.com>
2026-06-02 11:21:08 +0800

    亮點分析格式一

--- 亮點成果.py
+++ 亮點成果.py
@@ -52,3 +52,90 @@ for file in excel_files:
         print(e)
 
 print("全部完成！")
+
+
+#%%
+from pathlib import Path
+import pandas as pd
+
+input_folder = Path(r"C:/Users/0128M/OneDrive/桌面/智庫/week4/成果報告/新增第五層結果")
+output_folder = input_folder / "亮點分析"
+output_folder.mkdir(parents=True, exist_ok=True)
+
+excel_files = list(input_folder.glob("*.xlsx"))
+
+print("找到檔案數：", len(excel_files))
+
+for file in excel_files:
+    try:
+        df = pd.read_excel(file)
+
+        # 清理第四層文字
+        fourth = (
+            df["第四層"]
+            .astype(str)
+            .str.replace(" ", "", regex=False)
+            .str.replace("　", "", regex=False)
+            .str.replace("\n", "", regex=False)
+            .str.strip()
+        )
+
+        # 重點：抓「亮點」即可
+        df_filtered = df[fourth.str.contains("亮點", na=False)]
+
+        output_name = file.stem + "_亮點成果.xlsx"
+        output_path = output_folder / output_name
+
+        df_filtered.to_excel(output_path, index=False)
+
+        print(f"完成：{output_name}")
+        print(f"保留筆數：{len(df_filtered)}")
+
+    except Exception as e:
+        print(f"錯誤：{file.name}")
+        print(e)
+
+print("全部完成！")
+# %%
+from pathlib import Path
+import pandas as pd
+
+input_folder = Path(r"C:/Users/0128M/OneDrive/桌面/智庫/week4/成果報告/新增第五層結果/112到113")
+output_folder = input_folder / "亮點分析"
+output_folder.mkdir(parents=True, exist_ok=True)
+
+excel_files = list(input_folder.glob("*.xlsx"))
+
+print("找到檔案數：", len(excel_files))
+
+for file in excel_files:
+    try:
+        df = pd.read_excel(file)
+
+        # 清理第四層文字
+        fourth = (
+            df["第三層"]
+            .astype(str)
+            .str.replace(" ", "", regex=False)
+            .str.replace("　", "", regex=False)
+            .str.replace("\n", "", regex=False)
+            .str.strip()
+        )
+
+        # 重點：抓「亮點」即可
+        df_filtered = df[fourth.str.contains("亮點", na=False)]
+
+        output_name = file.stem + "_亮點成果.xlsx"
+        output_path = output_folder / output_name
+
+        df_filtered.to_excel(output_path, index=False)
+
+        print(f"完成：{output_name}")
+        print(f"保留筆數：{len(df_filtered)}")
+
+    except Exception as e:
+        print(f"錯誤：{file.name}")
+        print(e)
+
+print("全部完成！")
+# %%
